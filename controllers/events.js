934ef5461cf87ee08ae1f3b5caf4f6f05b3e9152
Christina <christinamarieelaine@gmail.com>
2026-05-07 19:41:58 -0700

    feat: add event index route

--- controllers/events.js
+++ controllers/events.js
@@ -1,6 +1,20 @@
 const router = require('express').Router();
 const Event = require('../models/event');
 
+// Index GET /events
+router.get('/', async (req, res) => {
+  try {
+    const events = await Event.find({})
+      .populate('creator', 'username')
+      .sort({ date: 1 });
+
+    res.status(200).json(events);
+  } catch (error) {
+    res.status(500).json({ err: error.message });
+  }
+});
+
+// Create POST /events
 router.post('/', async (req, res) => {
   try {
     const event = await Event.create({
